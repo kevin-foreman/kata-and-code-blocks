51f7253e567e9a625fdb70f139ed9e18c60dadeb
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-01-22 11:18:07 -0600

    test-app now has a fizzBuzz array example

--- test-app.js
+++ test-app.js
@@ -1,5 +1,29 @@
-let fun = function(input) {
-  console.log(input.toUpperCase());
+function fizzBuzzArray(input) {
+
+  let fizzArr = [];
+
+  for(let i = 1; i <= input; i++) {
+
+    if(i % 3 === 0 && i % 5 === 0) {
+
+      fizzArr.push('fizzBuzz');
+
+    } else if(i % 3 === 0) {
+
+      fizzArr.push('fizz');
+
+    } else if(i % 5 === 0) {
+
+      fizzArr.push('buzz');
+
+    } else {
+      fizzArr.push(i);
+    }
+
+  }
+
+  return fizzArr;
+
 };
 
-fun('hi');
+console.log(fizzBuzzArray(15));
